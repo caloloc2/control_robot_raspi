3a51ff05409e05f0e98aff0d8399efedeae68826
caloloc2 <calolomino@gmail.com>
2019-06-26 11:42:48 -0500

    actualiza

--- control.py
+++ control.py
@@ -1,9 +1,29 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT) ## GPIO 17 como salida
-GPIO.setup(27, GPIO.OUT) ## GPIO 27 como salida
+GPIO.setup(5, GPIO.OUT) ## GPIO 5 como salida
+GPIO.setup(6, GPIO.OUT) ## GPIO 6 como salida
+GPIO.setup(13, GPIO.OUT) ## GPIO 13 como salida
 
-print "si funca"
+GPIO.setup(22, GPIO.IN) ## GPIO 22 como entrada
+GPIO.setup(10, GPIO.IN) ## GPIO 10 como entrada
+GPIO.setup(9, GPIO.IN) ## GPIO 9 como entrada
+GPIO.setup(11, GPIO.IN) ## GPIO 11 como entrada
+
+print "Inicio programa..."
+
+ite = 0
+while ite<20:
+    GPIO.output(5, True)
+    GPIO.output(6, True)
+    GPIO.output(13, True)
+    time.sleep(0.5)
+    GPIO.output(5, False)
+    GPIO.output(6, False)
+    GPIO.output(13, False)
+    time.sleep(0.5)
+    ite += 2
+
+print "Fin programa."
 
 GPIO.cleanup()
